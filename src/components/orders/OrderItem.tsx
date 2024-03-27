be3aaf5bbcd4b6e5a0232d02c3cba280b3289d25
Matias Andrada <95193566+MatiasAndrada@users.noreply.github.com>
2024-03-27 23:02:43 +0000

    styles in orders

--- src/components/orders/OrderItem.tsx
+++ src/components/orders/OrderItem.tsx
@@ -1,19 +1,22 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-
+//Styles 
+import styles from "./styles/orderItem"
 //Types
 import type { CartItem } from "../../types";
 
 const OrderItem = ({ item }: { item: CartItem }) => {
-
+    const { title, price, quantity } = item
+    const priceAmount = price * quantity
 
     return (
-        <View>
-            <Text>Order Item</Text>
-        </View>
+        <View style={styles.cardContainer}>
+            <Text style={styles.titleText}>{title} </Text>
+            <Text style={styles.quantityText}>{quantity}</Text>
+            <Text style={styles.priceText}> {priceAmount} </Text>
+        </View >
     );
 };
 
 export default OrderItem;
 
-const styles = StyleSheet.create({});
---
+++ src/components/orders/styles/orderItem.ts
@@ -0,0 +1,42 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../../styles";
+const styles = StyleSheet.create({
+  cardContainer: {
+    height: 100,
+    padding: 20,
+    margin: 15,
+    borderWidth: 2,
+    borderRadius: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: 4,
+  },
+  /*
+  image: {
+    minHeight: 90,
+    minWidth: 90,
+    width: "30%",
+    borderRadius: 5,
+  },*/
+  titleText: {
+    flex: 3,
+    marginTop: 8,
+    fontSize: 22,
+    color: colors.textPrimary,
+  },
+  quantityText: {
+    flex: 1,
+    alignSelf: "flex-start",
+    marginTop: 10,
+    color: colors.textPrimary
+  },
+  priceText: {
+    flex: 1,
+    alignSelf: "flex-end",
+    marginTop: 10,
+    color: colors.textPrimary
+  },
+});
+
+export default styles;
